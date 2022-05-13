1f5d904c2a9cb1837b1bfcd73bbcf34b61c157b0
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-13 19:44:29 -0400

    added two helper routines for checking test status and creating the env file

--- setenv.sh
+++ setenv.sh
@@ -8,12 +8,15 @@ if ! [ -f ./setenv.sh ]; then
 fi
 
 export PORT_ROOT="${PWD}"
+unset PORT_TARBALL
+unset PORT_GIT
 export PORT_TARBALL="Y"
+#export PORT_GIT="Y"
 export PORT_TARBALL_URL="https://ftp.gnu.org/gnu/libtool/libtool-2.4.tar.gz"
 export PORT_TARBALL_DEPS="curl gzip make m4 perl autoconf"
 
 export PORT_GIT_URL="https://github.com/autotools-mirror/libtool.git"
-export PORT_GIT_DEPS="git make m4 perl autoconf"
+export PORT_GIT_DEPS="git make m4 perl autoconf automake"
 
 export PORT_EXTRA_CFLAGS=""
 export PORT_EXTRA_LDFLAGS=""
