cd4c0d920c7e0a8f06b95355ba7989268389095b
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:58 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'libtoolport'), string(name: 'DESCRIPTION', 'libtoolport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'libtoolport'), string(name: 'DESCRIPTION', 'GNU Libtool is a generic library support script that hides the complexity of using shared libraries behind a consistent, portable interface.' )]
         }
 }
