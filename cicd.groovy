6af9029b5a93a0aa794fd94634b13c2536c6e299
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 09:55:07 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'libtoolport'), string(name: 'DESCRIPTION', value: 'GNU Libtool is a generic library support script that hides the complexity of using shared libraries behind a consistent, portable interface.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'libtoolport'), string(name: 'PORT_DESCRIPTION', value: 'GNU Libtool is a generic library support script that hides the complexity of using shared libraries behind a consistent, portable interface.' )]
         }
 }
