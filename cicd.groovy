2c523bd68117fed4aa833b7b25d46fadec3ee415
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:29 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libtoolport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libtoolport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libtoolport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU Libtool is a generic library support script that hides the complexity of using shared libraries behind a consistent, portable interface.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libtoolport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU Libtool is a generic library support script that hides the complexity of using shared libraries behind a consistent, portable interface.' )]
         }
 }
